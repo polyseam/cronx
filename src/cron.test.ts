39be9e2716a52f13395c325c1c36133b0cc7e4fe
Matt Johnston <m@ttjohnston.com>
2025-03-11 08:37:47 -0300

    more test cases

--- src/cron.test.ts
+++ src/cron.test.ts
@@ -12,6 +12,16 @@ Deno.test("basic zero-to-one conversion: saturday as 7", () => {
   assertEquals(result, "0 0 * * 7");
 });
 
+Deno.test("basic zero-to-one conversion: thursday,sunday", () => {
+  const result = convertZeroBasedDaysToOneBased("0 0 * * 4,6");
+  assertEquals(result, "0 0 * * 5,7");
+});
+
+Deno.test("basic zero-to-one conversion: sunday to wednesday", () => {
+  const result = convertZeroBasedDaysToOneBased("0 0 * * 0-3");
+  assertEquals(result, "0 0 * * 1-4");
+});
+
 Deno.test("basic crontab localisation", () => {
   const result = convertCronToUTC("0 0 * * 0", 0);
   assertEquals(result, "0 0 * * 0");
@@ -20,4 +30,14 @@ Deno.test("basic crontab localisation", () => {
 Deno.test("basic crontab localisation", () => {
   const result = convertCronToUTC("0 14 * * 0", -3);
   assertEquals(result, "0 17 * * 0");
+});
+
+Deno.test("crontab localisation with ranges", () => {
+  const result = convertCronToUTC("0 14-18 * * 0", -3);
+  assertEquals(result, "0 17-21 * * 0");
+});
+
+Deno.test("crontab localisation with list", () => {
+  const result = convertCronToUTC("0 12,14,15 * * 0", -3);
+  assertEquals(result, "0 15,17,18 * * 0");
 });
